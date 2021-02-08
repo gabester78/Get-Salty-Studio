56f85be512efb0b8704b2cec595adcec373415ff
Gabriel Romero <gabester78@gmail.com>
2021-02-08 13:12:34 -0600

    Revised responsive breakpoints to percentages for nav footer and home components.

--- gs-studio/src/components/about.js
+++ gs-studio/src/components/about.js
@@ -14,13 +14,13 @@ const About = () => {
             <section id="top" className="profile_container"> 
               <img className="headshot" src={Shannon} alt="headshot of Shannon"/>
               <div className="profile">
-                <h1>Shannon - Owner &amp; Esthetician</h1>
+                <h1 className="profile_h1">Shannon - Owner &amp; Esthetician</h1>
                 <img className="border" src={teal_border} alt="wavy teal line graphic" />
                 <p>Shannon Nolan, Licensed Esthetician since 1995. Specializing in Botanical Skincare, 
                   Rose Quartz Facials, &amp; teen acne. She is also a Certified Aromatherapist &amp; Certified Master 
                   Herbalist all of which is incorporated into her Holistic Skincare Practice. Shannon opened Get 
                   Salty Skincare Studio to not only create a relaxing one of a kind space for her clients, but to 
-                  be able to provide an affordable alternative/complimentary therapy within her studio for guests 
+                  be able to provide an affordable alternative &amp; complimentary therapy within her studio for guests 
                   with a variety of breathing &amp; skin conditions. The idea of the Salt Room stemmed from wanting 
                   to help her Father who has COPD &amp; her son who has terrible allergies. She wanted to have a place 
                   to share the benefits that the salt room provides for them with her family of clients as well. 
@@ -30,7 +30,7 @@ const About = () => {
             <section className="profile_container">
               <img className="headshot2" src={Heather} alt="headshot of Heather"/>
               <div className="profile">
-                <h1>Heather - Licensed Massage Therapist</h1>
+                <h1 className="profile_h1">Heather - Licensed Massage Therapist</h1>
                 <img className="border" src={blue_border} alt="wavy teal line graphic" />
                 <p>Heather Schultz, licensed massage therapist, graduated from Chicago’s The SOMA 
                   institute: School of Clinical Massage Therapy, in 2002. Working with chiropractors 
@@ -41,7 +41,7 @@ const About = () => {
             <section className="profile_container">
               <img className="headshot" src={Becca} alt="headshot of Becca"/>
               <div className="profile">
-                <h1>Becca - Licensed Massage Therapist</h1>
+                <h1 className="profile_h1">Becca - Licensed Massage Therapist</h1>
                 <img className="border" src={teal_border} alt="wavy teal line graphic" />
                 <p>Becca graduated 
                   from Cortiva massage school in 2014 &amp; is currently finishing her degree to be a Physical Therapist Assistant 
@@ -55,7 +55,7 @@ const About = () => {
             <section className="profile_container">
               <img className="headshot2" src={Celena} alt="headshot of Celena"/>
               <div className="profile">
-                <h1>CeLena - Licensed Nail Technician</h1>
+                <h1 className="profile_h1">CeLena - Licensed Nail Technician</h1>
                 <img className="border" src={blue_border} alt="wavy teal line graphic" />
                 <p>CeLena provides the highest level of customer service. 
                   She is a perfectionist and recommends the best colors, designs, and treatments for you. 
