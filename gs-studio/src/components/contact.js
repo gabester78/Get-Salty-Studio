b4d6728ece63eae425fe4d2e6d19743e807e1322
Gabriel Romero <gabester78@gmail.com>
2021-11-17 16:47:20 -0600

    Added title to map frame.

--- gs-studio/src/components/contact.js
+++ gs-studio/src/components/contact.js
@@ -55,6 +55,7 @@ const Contact = () => {
           </div>
           <div className="map_container">
             <iframe
+              title="studioMap"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1474.5516391484023!2d-88.28458101638036!3d42.340321399290744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f7100402fe417%3A0x6c3753cf5d6a1f94!2s4318%20W%20Crystal%20Lake%20Rd%2C%20McHenry%2C%20IL%2060050!5e0!3m2!1sen!2sus!4v1637185281762!5m2!1sen!2sus"
               width="400"
               height="300"
